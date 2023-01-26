58da669333912e60d7aa17c5255243bb4aea6833
Your Name <you@example.com>
2023-01-26 15:15:52 -0500

    picture and icon

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import {useFonts} from 'expo-font'
+import Icon from './weatherIcon';
 import WeatherDetail from './components/WeatherDetail';
 import WeatherHourly from './components/WeatherHourly';
 import WeatherSummary from './components/WeatherSummary';
 
 export default function App() {
+
+  const [loaded] = useFonts (
+
+    { icomoon: require('./assets/fonts/icomoon.ttf') }
+  );
+
+  if(!loaded) {
+    return null;
+  }
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       
       <WeatherSummary />
-
+      <Icon name="wi-night-alt-snow" style={styles.icon} />    
       <WeatherDetail />
 
       <WeatherHourly />
@@ -30,5 +42,10 @@ const styles = StyleSheet.create({
   },
   Text: {
     color: '#fff'
+  },
+
+  icon: {
+    color: '#fff',
+    fontSize: 74
   }
 });
--- components/WeatherSummary.js
+++ components/WeatherSummary.js
@@ -1,16 +1,64 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image} from 'react-native'
 import React from 'react'
+import heroImage from '../assets/moon-Cloud-cropped.jpg';
+
 
 export default function WeatherSummary() {
   return (
-    <View>
-      <Text style={styles.text}>WeatherSummary</Text>
+    <View style={styles.heroContainer}>
+      <Text style={styles.text}>Good Afternoon</Text>
+      <Image source={heroImage} style={styles.hero} />
+      <View style={styles.tempContainer}>
+        <Text style={styles.highTemp}>46{'\u00b0'}F</Text>
+        <Text style={styles.lowTemp}>/33{'\u00b0'}F</Text>
+
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     text: {
-        color: '#fff'
+        width: '90%',
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: '600',
+        marginTop: 10,
+        marginBottom: 8
+    },
+
+    tempContainer: {
+      flexDirection: 'row',
+      width: '90%',
+      justifyContent: 'flex-start',
+      alignItems: 'center'
+    },
+
+    highTemp:{
+        color: 'white',
+        fontSize:48,
+        fontWeight: '100',
+        marginRight: 8
+    },
+
+    lowTemp: {
+        color: 'lightblue',
+        fontSize:33,
+        fontWeight: '100'
+    },
+
+    heroContainer: {
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center'
+      },
+
+    hero: {
+      width: '100%',
+      height: undefined,
+      aspectRatio: 1,
+      borderRadius: 20,
+      resizeMode: 'cover'
+
       }
 })
